ef44f600424500685aa05742acf6be93efd0ecba
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2018-10-13 20:53:22 +0200

    add: simple layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 // Add FontAwesome library
 require('Resources/libs/js/fontawesome.min.js');
-
-console.log('Webpack');
-
-if (process.env.NODE_ENV === 'production') {
-   console.log('fsdfsdfsdfs');
-}
+require('Styles/index.scss');
