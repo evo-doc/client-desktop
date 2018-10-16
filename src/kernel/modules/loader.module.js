1fa9dd4df9036c9c8707177555ce0bcc85f853d2
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2018-10-16 14:15:40 +0200

    add: module - loader

---
+++ src/kernel/modules/loader.module.js
@@ -0,0 +1,28 @@
+/**
+ * Loader - globally required modul via webpack. Reserves `loader` as a global variable.
+ * Provides an interface of loader screen.
+ * @module Loader
+ *
+ * @example <caption>Show & Hide</caption>
+ * loader.show();
+ * // some long-time actions
+ * loader.hide();
+ */
+
+const loader = document.getElementById('loader');
+
+/**
+ * @summary Show loader
+ * @description Show loader prevents user to click. E.g. during ajax requests.
+ */
+module.exports.show = () => {
+   loader.setAttribute('class', 'loader_visible');
+};
+
+/**
+ * @summary Hide loader
+ * @description Hide loader.
+ */
+module.exports.hide = () => {
+   loader.setAttribute('class', 'loader_hidden');
+};
