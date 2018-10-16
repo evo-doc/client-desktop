e251f2d34172ac2a94fa4875db85ebdb7a0eb3fd
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2018-10-16 14:17:27 +0200

    add: modules - logger placeholders

---
+++ src/kernel/modules/logger.api.module.js
@@ -0,0 +1,17 @@
+module.exports = {
+   trace(message) {
+      console.log(message);
+   },
+   info(message) {
+      console.log(message);
+   },
+   debug(message) {
+      console.log(message);
+   },
+   warn(message) {
+      console.log(message);
+   },
+   error(message) {
+      console.log(message);
+   },
+};
