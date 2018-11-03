c0fc56e7888e5e55970786d463bef0935d16f977
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2018-11-03 18:53:21 +0100

    update: signup validation

--- src/local/routes/auth/signup/index.js
+++ src/local/routes/auth/signup/index.js
@@ -1,8 +1,9 @@
 // Stylesheet
 require('./index.scss');
 
-
+const Noty = require('noty');
 const Page = require('Kernel/Page.class');
+const validator = require('Modules/validation.module');
 const template = require('./index.pug');
 
 
@@ -18,7 +19,65 @@ class Index extends Page {
 
    __ajaxData() {}
 
-   __handlers() {}
+   __handlers() {
+      document.getElementById('authorization__form_signup').addEventListener('submit', (e) => {
+         e.preventDefault();
+         if (
+            this._usernameValidation()
+            && this._emailValidation()
+            && this._passwordValidator()
+         ) {
+            // TODO: Send request
+            new Noty({
+               type: 'success',
+               text: 'Form submitted.',
+            }).show();
+         }
+      });
+   }
+
+   _passwordValidator() {
+      const password = document.getElementById('authorization__signup_password').value;
+
+      if (!validator.password(password)) {
+         new Noty({
+            type: 'error',
+            timeout: 7000,
+            text: 'The password is invalid. It shoud contain at least 1 lowercase, 1 uppercase, 1 number, 1 of !@#$%^&* symbols.',
+         }).show();
+         return false;
+      }
+
+      return true;
+   }
+
+   _emailValidation() {
+      const email = document.getElementById('authorization__signup_email').value;
+      if (!validator.email(email)) {
+         new Noty({
+            type: 'error',
+            timeout: 7000,
+            text: 'The e-mail is invalid.',
+         }).show();
+         return false;
+      }
+
+      return true;
+   }
+
+   _usernameValidation() {
+      const username = document.getElementById('authorization__signup_username').value;
+      if (!validator.username(username)) {
+         new Noty({
+            type: 'error',
+            timeout: 7000,
+            text: 'The username is invalid. It may contain only a-Z, numbers and symbols -_.',
+         }).show();
+         return false;
+      }
+
+      return true;
+   }
 }
 
 module.exports = (config = {}) => ({
