76ea7aaba154e5f6f50a7050e618d4223be80b04
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2018-12-03 21:08:42 +0100

    fix: stats api

--- src/kernel/Api.class.js
+++ src/kernel/Api.class.js
@@ -3,6 +3,7 @@ const log = require('Modules/logger.app.module');
 
 // API Modules
 const AuthAPI = require('Modules/api/auth.module');
+const StatsAPI = require('Modules/api/stats.module');
 
 
 class API {
@@ -10,15 +11,22 @@ class API {
       log.info('API Initializer');
 
       this._auth = null;
+      this._stats = null;
    }
 
    init() {
       this.getAuth().init();
+      this.getStats();
    }
 
    getAuth() {
       if (this._auth === null) this._auth = new AuthAPI();
       return this._auth;
+   }
+
+   getStats() {
+      if (this._stats === null) this._stats = new StatsAPI();
+      return this._stats;
    }
 }
 
