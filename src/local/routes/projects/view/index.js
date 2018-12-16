744a76136cecefedcc92ff9b5f58e3a83bcc051a
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2018-12-16 03:01:19 +0100

    update: user projects view

--- src/local/routes/projects/view/index.js
+++ src/local/routes/projects/view/index.js
@@ -24,7 +24,7 @@ class Index extends Page {
       try {
          await Promise
             .all([
-               evodoc.getAPI().getUserPrivate().getAccessibleProjects(5),
+               evodoc.getAPI().getUserPrivate().getAccessibleProjects(),
             ])
             .then((values) => {
                [
@@ -38,6 +38,16 @@ class Index extends Page {
    }
 
    __render() {
+      // Onwer tags
+      for (let i = 0; i < this._getAccessibleProjects.body.projects.data.length; i++) {
+         const owner = this._getAccessibleProjects.body.projects.data[i][1];
+         const username = evodoc.getAPI().getAuth().getUsername();
+         if (owner === username) {
+            this._getAccessibleProjects.body.projects.data[i][1] = true;
+         }
+      }
+
+
       this._getRenderParent().innerHTML = this._template({
          projects: this._getAccessibleProjects.body.projects.data,
       });
