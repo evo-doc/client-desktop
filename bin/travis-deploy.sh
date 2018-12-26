2c342bbc21963ae642c886332400ff382a359d86
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2018-12-26 19:39:45 +0100

    wip: travis windows deploy

--- bin/travis-deploy.sh
+++ bin/travis-deploy.sh
@@ -2,3 +2,5 @@ yarn packager:mac
 tar -cvzf "release/EvoDoc-macOS-x64.tar.gz" -C "release/EvoDoc-darwin-x64/" "EvoDoc.app"
 yarn packager:lin
 tar -cvzf "release/EvoDoc-linux-x64.tar.gz" -C "release/EvoDoc-linux-x64" .
+yarn packager:win
+tar -cvzf "release/EvoDoc-windows-x64.tar.gz" -C "release/EvoDoc-windows-x64" .
